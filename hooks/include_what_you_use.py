afd84855d198169f9bc5f8cc96f2a883f34b2bf6
Takishima <damien1@huawei.com>
2021-09-01 02:02:24 +0000

    Adding iwyu of takishima from #28

---
+++ hooks/include_what_you_use.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Wrapper for include-what-you-use"""
+import shutil
+import sys
+
+from hooks.utils import Command, StaticAnalyzerCmd
+
+
+class IncludeWhatYouUseCmd(StaticAnalyzerCmd):
+    """Class for the Include-What-You-Use command."""
+
+    command = "include-what-you-use"
+    lookbehind = "include-what-you-use "
+
+    def __init__(self, args):
+        super().__init__(self.command, self.lookbehind, args)
+        self.check_installed()
+        self.parse_args(args)
+
+    def run(self):
+        """Run Include-What-You-Use. Error if diff is incorrect. "Correct" """
+
+        for filename in self.files:
+            self.run_command([filename] + self.args)
+            is_correct = b"has correct #includes/fwd-decls" in self.stderr
+            if is_correct:
+                self.returncode = 0
+                self.stdout = b''
+                self.stderr = b''
+            else:
+                sys.stderr.buffer.write(self.stdout + self.stderr)
+                sys.exit(self.returncode)
+
+
+def main(argv=None):
+    cmd = IncludeWhatYouUseCmd(argv)
+    cmd.run()
+
+
+if __name__ == "__main__":
+    main()
